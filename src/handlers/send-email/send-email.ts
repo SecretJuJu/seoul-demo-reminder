04a414dd9f48fd3101113aaf925e45950b3f1bbe
secretjuju <dev.tmdqh@gmail.com>
2024-07-12 15:26:03 +0900

    feat: 이메일을 한번에 발송하도록 수정

--- src/handlers/send-email/send-email.ts
+++ src/handlers/send-email/send-email.ts
@@ -5,6 +5,7 @@ import { getEnv } from '../../common/utils';
 import { getMessageFromEvent } from '../../common/sqs';
 import { DividedNoticeEmailChunk } from '../divide-emails/divide-notice-emails';
 import { alertError } from '../../common/alert';
+import Mail from 'nodemailer/lib/mailer';
 
 export const handler = async (event: SQSEvent) => {
   const transporter = nodeMailer.createTransport({
@@ -18,21 +19,16 @@ export const handler = async (event: SQSEvent) => {
   const message = getMessageFromEvent(event)[0];
   const { emails, contents } = JSON.parse(message) as DividedNoticeEmailChunk;
 
-  // contents 별로 각각의 customer 에게 email 보내기
-  for (const email of emails) {
-    const mailOptions = {
-      from: getEnv('EMAIL_SENDER_ADDRESS'),
-      to: email,
-      subject: '서울시 집회/행사 알림',
-      html: contents,
-    };
+  const mailOptions: Mail.Options = {
+    from: getEnv('EMAIL_SENDER_ADDRESS'),
+    to: emails,
+    subject: '서울시 집회/행사 알림',
+    html: contents,
+  };
 
-    try {
-      await transporter.sendMail(mailOptions);
-    } catch (err) {
-      await alertError(err);
-    }
-    // sleep for 0.5 sec
-    await new Promise((resolve) => setTimeout(resolve, 500));
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    await alertError(err);
   }
 };
